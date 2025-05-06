9d6b00abc4288a6485a9ab5ae4250ae4e2b4cd48
kangson896 <kangson896@gmail.com>
2025-05-06 20:11:23 +0800

    hello first home

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import mediapipe as mp
 import pyautogui
 
 # 初始化MediaPipe面部关键点检测
-mp_face_mesh = mp.solutions.face_mesh
+# test git
+mp_face_mesh = mp.solutions.face_mesh 
 face_mesh = mp_face_mesh.FaceMesh(
     min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_faces=1
 )
